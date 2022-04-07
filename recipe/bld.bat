3ffe51784cb19db2e6547e58f21b303ca34d00f2
Mark Harfouche <mark.harfouche@gmail.com>
2022-04-07 12:38:27 -0400

    Disale tests on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,7 @@ cmake .. -GNinja ^
 -DCMAKE_INSTALL_LIBDIR=lib ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DBUILD_SHARED_LIBS=ON ^
--DAVIF_BUILD_TESTS=ON ^
+-DAVIF_BUILD_TESTS=OFF ^
 -DAVIF_CODEC_AOM=ON ^
 -DAVIF_CODEC_SVT=OFF ^
 -DAVIF_CODEC_DAV1D=OFF ^
@@ -17,12 +17,6 @@ cmake .. -GNinja ^
 if errorlevel 1 exit /b 1
 
 ninja
-if errorlevel 1 exit /b 1
-
-:: 2021/12/05 hmaarrfk
-:: Tests are a little flaky, and are disabled upstream
-:: https://github.com/AOMediaCodec/libavif/issues/798
-.\aviftest ..\tests\data\ --io-only
 if errorlevel 1 exit /b 1
 
 ninja install
