05002ae073691f75e79e95948d0ca06b1065a6d4
Mark Harfouche <mark.harfouche@gmail.com>
2022-04-08 07:30:04 -0400

    Treat all warnigns as error with upstream patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,12 @@ mkdir build
 cd build
 if errorlevel 1 exit /b 1
 
-:: Disable warning as errors due to the described error in
-:: https://github.com/conda-forge/libavif-feedstock/pull/5#issuecomment-1091825097
-
 :: Other codecs cannot be enabled because they are not on conda-forge
 cmake .. -GNinja                                 ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"    ^
   -DCMAKE_INSTALL_LIBDIR=lib                     ^
   -DCMAKE_BUILD_TYPE=Release                     ^
-  -DAVIF_ENABLE_WERROR=OFF                       ^
   -DBUILD_SHARED_LIBS=ON                         ^
   -DAVIF_BUILD_TESTS=OFF                         ^
   -DAVIF_CODEC_AOM=ON                            ^
