8902f0195efe0d4605550ec58f6ae4dd15ef5c16
Mark Harfouche <mark.harfouche@gmail.com>
2022-04-07 22:18:04 -0400

    Try disabling the problematic warning

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,18 +2,22 @@ mkdir build
 cd build
 if errorlevel 1 exit /b 1
 
+:: https://github.com/conda-forge/libavif-feedstock/pull/5#issuecomment-1091825097
+set CFLAGS="%CFLAGS% /wd4204"
+echo %CFLAGS%
+
 :: Other codecs cannot be enabled because they are not on conda-forge
 cmake .. -GNinja ^
--DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
--DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
--DCMAKE_INSTALL_LIBDIR=lib ^
--DCMAKE_BUILD_TYPE=Release ^
--DBUILD_SHARED_LIBS=ON ^
--DAVIF_BUILD_TESTS=OFF ^
--DAVIF_CODEC_AOM=ON ^
--DAVIF_CODEC_SVT=OFF ^
--DAVIF_CODEC_DAV1D=OFF ^
--DAVIF_CODEC_LIBGAV1=OFF
+  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_INSTALL_LIBDIR=lib ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DBUILD_SHARED_LIBS=ON ^
+  -DAVIF_BUILD_TESTS=OFF ^
+  -DAVIF_CODEC_AOM=ON ^
+  -DAVIF_CODEC_SVT=OFF ^
+  -DAVIF_CODEC_DAV1D=OFF ^
+  -DAVIF_CODEC_LIBGAV1=OFF
 if errorlevel 1 exit /b 1
 
 ninja
