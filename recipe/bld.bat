e30141cc6ae9c66ed6c702645446619a5ad3a96e
Daniel Ching <carterbox@users.noreply.github.com>
2023-08-30 13:01:38 -0500

    BUG: Disable rav1e in the build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,7 +14,7 @@ cmake .. -GNinja                                 ^
   -DAVIF_CODEC_SVT=ON                           ^
   -DAVIF_CODEC_DAV1D=ON                          ^
   -DAVIF_CODEC_LIBGAV1=OFF                       ^
-  -DAVIF_CODEC_RAV1E=ON
+  -DAVIF_CODEC_RAV1E=OFF
 
 if errorlevel 1 exit /b 1
 
